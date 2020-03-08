6062e43136c9d3ededba3e59a99e208234dd6407
richelbilderbeek <richel@richelbilderbeek.nl>
2020-03-08 14:31:48 +0100

    AppVeyor does less

--- example_5.R
+++ example_5.R
@@ -9,7 +9,7 @@
 library(pirouette)
 library(beautier)
 # Constants
-is_testing <- is_on_travis()
+is_testing <- is_on_ci()
 example_no <- 5
 rng_seed <- 314
 folder_name <- paste0("example_", example_no, "_", rng_seed)
